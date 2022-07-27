3905876170e971c49266384957a1960ea460cd85
CXTNUCE98 <79709491+CXTNUCE98@users.noreply.github.com>
2022-07-27 22:45:21 +0700

    get start

---
+++ src/components/Header.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { BsWatch } from 'react-icons/bs';
+import {AiOutlineClose} from 'react-icons/ai'
+import {BsMoon, BsGrid} from 'react-icons/bs'
+import {RiShoppingBagLine} from 'react-icons/ri'
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <div className='header'>
+      <nav className="nav container">
+        <Link to='/'>
+          <div className='nav__logo'>
+            <BsWatch/>Rolex
+          </div>
+        </Link>
+        <div className="nav__menu">
+          <ul className="nav__list">
+            <li className="nav__item">
+              <a href="#Home" className='nav__link active-link'>Home</a>
+            </li>
+            <li className="nav__item">
+              <a href="#Featured" className='nav__link '>Featured</a>
+            </li>
+            <li className="nav__item">
+              <a href="#Products" className='nav__link '>Products</a>
+            </li>
+            <li className="nav__item">
+              <a href="#News" className='nav__link '>News</a>
+            </li>
+          </ul>
+          <div className="nav__close">
+            <AiOutlineClose />
+          </div>
+        </div>
+
+        <div className="nav__btns">
+          <BsMoon />
+          <div className="nav__shop">
+            <RiShoppingBagLine />
+          </div>
+          <div className="nav__shop">
+            <BsGrid />
+          </div>
+        </div>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
